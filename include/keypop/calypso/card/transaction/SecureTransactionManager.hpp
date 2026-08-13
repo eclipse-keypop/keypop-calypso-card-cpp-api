f8b2fabe43350b82e9d528103c641ad0a5b84e9d
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2026-08-13 22:18:31 +0200

    wip

--- include/keypop/calypso/card/transaction/SecureTransactionManager.hpp
+++ include/keypop/calypso/card/transaction/SecureTransactionManager.hpp
@@ -42,7 +42,7 @@ public:
      * @throw IllegalArgumentException If the provided class is null.
      * @since 2.0.0
      */
-    virtual std::shared_ptr<CardTransactionCryptoExtention> getCryptoExtension()
+    virtual std::shared_ptr<CardTransactionCryptoExtension> getCryptoExtension()
         = 0;
 
     /**
